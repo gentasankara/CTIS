77ff6597a86c5daf420d028a2ca5bc1bbe060f68
gentasankara <62679608+gentasankara@users.noreply.github.com>
2021-03-26 13:12:27 +0800

    Manage Test Kit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index(Request $request)
+    {
+        if($request->has('cari')){
+            $data_test = \App\TestKit::where('kitName','LIKE','%'.$request->cari.'%')->get();
+        }
+        else
+        {
+            $data_test = \App\TestKit::all();
+        }
+        $data_centre = \App\TestCentre::all();
+        $title= "Test Kit Manage";
+        return view('testKit.index', ['data_test' => $data_test,'data_centre'=>$data_centre, 'title' =>$title]);
+    }
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'kitName' =>'required',
+            'test_centre_id' =>'required',
+            'stock' =>'required|integer'
+        ]  
+        );
+        \App\TestKit::create($request->all());
+        return redirect('/testKit')->with('success','Successfully record new data');
+    }
+    public function edit($id)
+    {
+        $test = \App\TestKit::find($id);
+        $title= "Edit Test Kit ";
+        return view('testKit/edit',['test' => $test, 'title' =>$title]);
+    }
+    public function update(Request $request, $id)
+    {   
+        $this->validate($request, [
+            'kitName' =>'required',
+            'stock' =>'required|integer'
+        ]  
+        );
+        $test = \App\TestKit::find($id);
+        $test->update($request->all());
+        return  redirect('/testKit')->with('success','Successfully update the data');
+    }
+    public function delete($id)
+    {
+        $test = \App\TestKit::find($id);
+        $test->delete($test);
+        return redirect('/testKit')->with('success','Data Removed');
+    }
+}
